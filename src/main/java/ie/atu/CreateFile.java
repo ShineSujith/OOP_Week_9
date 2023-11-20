adc756240749f6ce22fc8a85a1a2618d6720cd0f
Shine Sujith <G00414454@atu.ie>
2023-11-20 16:33:26 +0000

    added code to create a new file

--- src/main/java/ie/atu/CreateFile.java
+++ src/main/java/ie/atu/CreateFile.java
@@ -5,6 +5,24 @@ import java.io.IOException;
 
 public class CreateFile {
     public static void main(String[] args) {
+        //specify the file name
+        String fileName = "File13.txt";
 
+        //create a new file object
+        File myFile = new File(fileName);
+
+        try {
+            //Create a new file on the disk
+            if (myFile.createNewFile()){
+                System.out.println("File created successfully at " + myFile.getAbsolutePath());
+            }
+            else {
+                System.out.println("File already exists at " + myFile.getAbsolutePath());
+            }
+        }
+        catch (IOException e){
+            System.out.println("An error has occurred while creating the file.");
+            e.printStackTrace();
+        }
     }
 }
