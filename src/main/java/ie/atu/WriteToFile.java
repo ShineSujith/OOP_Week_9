1905ff7d9a28b7787c1d1a4e6330ad32579155fd
Shine Sujith <G00414454@atu.ie>
2023-11-20 17:02:12 +0000

    added code to write to a file

---
+++ src/main/java/ie/atu/WriteToFile.java
@@ -0,0 +1,19 @@
+package ie.atu;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteToFile {
+    public static void main(String[] args) {
+        //specify the file name
+        String fileName = "File13.txt";
+
+        try (FileWriter writer = new FileWriter(fileName);) {
+            writer.write("Some more content for the file");
+            System.out.println("Content Successfully added to file");
+        } catch (IOException e) {
+            System.out.println("An error has occurred while writing to the file.");
+            e.printStackTrace();
+        }
+    }
+}
